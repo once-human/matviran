f5df073a0639dcb87841a9ba8cc8e733d3c09c24
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-08-23 01:26:01 +0530

    header js

---
+++ public/assets/js/header.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
