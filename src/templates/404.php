6972babe14008b6521061f98daa6508e36bac0a9
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-08-22 23:09:26 +0530

    404 update

--- src/templates/404.php
+++ src/templates/404.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html style="height:100%">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>404 Not Found</title>
+    <style>
+        @media (prefers-color-scheme:dark) {
+            body {
+                background-color: #000 !important;
+            }
+        }
+    </style>
 </head>
-<body>
-    <h1>404</h1>
+
+<body style="color: #444; margin:0;font: normal 14px/20px Arial, Helvetica, sans-serif; height:100%; background-color: #fff;">
+    <div style="height:auto; min-height:100%;">
+        <div style="text-align: center; width:800px; margin-left: -400px; position:absolute; top: 30%; left:50%;">
+            <h1 style="margin:0; font-size:150px; line-height:150px; font-weight:bold;">404</h1>
+            <h2 style="margin-top:20px;font-size: 30px;">Page Not Found</h2>
+            <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+        </div>
+    </div>
 </body>
+
 </html>
