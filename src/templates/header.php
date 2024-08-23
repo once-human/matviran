a0fcd218242cf6b19e4a2d7ec9c62467d6d50f5d
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-08-23 12:29:17 +0530

    ui/ux update for header

--- src/templates/header.php
+++ src/templates/header.php
@@ -12,30 +12,38 @@
 <body>
     <header>
         <!-- Navbar Start -->
-        <nav class="navbar">
+        <nav class="navbar" aria-label="Main Navigation">
             <div class="logo">
-                <a href="/home">Matviran</a>
+                <a href="/home" aria-label="Matviran Homepage">Matviran</a>
             </div>
-            <div class="menu-toggle" id="mobile-menu">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </div>
-            <ul class="nav-links" id="nav-links">
-                <li><a href="/home" class="nav-item">Home</a></li>
-                <li><a href="/services" class="nav-item">Services</a></li>
-                <li><a href="/portfolio" class="nav-item">Portfolio</a></li>
+            <button class="menu-toggle" id="mobile-menu" aria-label="Toggle Menu" aria-expanded="false">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </button>
+            <ul class="nav-links" id="nav-links" role="menu">
+                <li><a href="/home" class="nav-item" role="menuitem">Home</a></li>
+                <li><a href="/services" class="nav-item" role="menuitem">Services</a></li>
+                <li><a href="/portfolio" class="nav-item" role="menuitem">Portfolio</a></li>
                 <li class="dropdown">
-                    <a href="#" class="nav-item">About Us <svg class="dropdown-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 9l-6 6-6-6z"/></svg></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/our-story" class="nav-item">Our Story</a></li>
-                        <li><a href="/team" class="nav-item">Meet the Team</a></li>
+                    <a href="#" class="nav-item" role="menuitem">About Us 
+                        <svg class="dropdown-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+                            <path d="M7.41 8.59L12 13.17l4.59-4.58L18 9l-6 6-6-6z"/>
+                        </svg>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="/our-story" class="nav-item" role="menuitem">Our Story</a></li>
+                        <li><a href="/team" class="nav-item" role="menuitem">Meet the Team</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="nav-item">Contact <svg class="dropdown-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 9l-6 6-6-6z"/></svg></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="/faqs" class="nav-item">FAQ</a></li>
+                    <a href="#" class="nav-item" role="menuitem">Contact 
+                        <svg class="dropdown-icon" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+                            <path d="M7.41 8.59L12 13.17l4.59-4.58L18 9l-6 6-6-6z"/>
+                        </svg>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="/faqs" class="nav-item" role="menuitem">FAQ</a></li>
                     </ul>
                 </li>
             </ul>
