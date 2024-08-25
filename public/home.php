9ca49a9f61812050adaad0eeb8cbddd69d741282
Onkar Yaglewad <121476278+once-human@users.noreply.github.com>
2024-08-25 20:05:04 +0530

    home page updated backend and metadata

--- public/home.php
+++ public/home.php
@@ -1,12 +1,16 @@
 <?php
 $pageTitle = 'Home'; // Set the page title dynamically
-include 'src/templates/header.php'; 
+include '../src/templates/header.php'; 
 ?>
 
+<link rel="header" href="../src/templates/header.php">
 <!-- Main content of the page -->
 <main>
     <h1>Welcome to Our Home Page</h1>
     <p>This is the content of the home page.</p>
 </main>
+
+
+
 
 <?php include 'footer.php'; ?>
